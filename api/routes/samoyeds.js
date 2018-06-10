1feb01c73e12a16618a6db3517bce75632477b5f
Evan Milton <vanthemilton@gmail.com>
2018-06-09 19:57:58 -0700

    did some endpoints, hope they work lel

--- api/routes/samoyeds.js
+++ api/routes/samoyeds.js
@@ -1,29 +1,65 @@
 const router = require('express').Router();
 const fs = require('fs');
 const multer = require('multer');
+const ObjectId = require('mongodb').ObjectId;
 
 const saveImage = multer({
     dest: __dirname + '../i/',
     limits: {fileSize: 10000000, files:1},
 });
 
+getRandomSamoyedLinks(count, mongoDB) {
+  const samoyeds = mongoDB.collection('samoyeds');
+  const query = { $sample: { size: count } };
+  return samoyeds.aggregate(query).toArray();
+}
 
-
-//returns up to 10 samoyed links TODO: query the mongo database, NOT the directory.
+// returns up to 10 samoyed links from mongo
 router.get('/', (req, res) =>{
   const mongo = req.app.locals.mongoDB;
   const count = parseInt(req.params.count);
-
-
-
+  if (!count || count < 1 || count > 10) {
+    count = 1;
+  }
+  getRandomSamoyedLinks(count, mongoDB)
+    .then((array) => {
+        res.status(201).json({
+          image_links: array
+        });
+      })
+    .catch((err) => {
+      res.status(500).json({
+        error: "Failed to fetch links."
+      });
+    });
 });
 
-//returns photo with given id TODO: grab from the directory.
+checkIfLinkExists(sid, mongoDB) {
+  const samoyeds = mongoDB.collection('samoyeds');
+  const query = { _id: new ObjectId(sid) };
+  return samoyeds.find(query).toArray();
+}
+
+//returns photo with given id
 router.get('/:SID', (req, res) =>{
   const mongo = req.app.locals.mongoDB;
   const count = parseInt(req.params.count);
-
-
+  checkIfLinkExists()
+    .then((result) => {
+      if (result[0]) {
+        let id = result[0];
+        res.status(201).sendFile(`../i/${result[0]}`);
+      }
+      else {
+        next();
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: "Failed to fetch photo."
+      });
+    });
+    })
 
 });
 
