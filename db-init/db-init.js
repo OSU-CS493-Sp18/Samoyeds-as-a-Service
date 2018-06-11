d9da972eaef29bd5067fa67abcaff6de3c2cf2ce
Evan Milton <vanthemilton@gmail.com>
2018-06-10 20:48:26 -0700

    default images and db entries

--- db-init/db-init.js
+++ db-init/db-init.js
@@ -2,4 +2,16 @@ db.createUser({
   user: "users",
   pwd: "trevor69",
   roles: [ { role: "readWrite", db: "users" } ]
-})
+});
+
+// ID for default images is 24 of the same number, to make naming default files not insane
+for (let i = 0; i < 8; i++) {
+    let objID = "";
+    for (let j = 0; j < 24; j++) {
+        objID += String(i);
+    }
+    db.samoyeds.insert({
+        _id: ObjectId(objID)
+    });
+}
+
