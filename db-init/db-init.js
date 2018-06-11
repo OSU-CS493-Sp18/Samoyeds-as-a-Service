16f153fc5234d43c4a3abdcdee0adf8ff0fde896
Evan Milton <vanthemilton@gmail.com>
2018-06-10 23:39:59 -0700

    functional but butts

--- db-init/db-init.js
+++ db-init/db-init.js
@@ -4,14 +4,17 @@ db.createUser({
   roles: [ { role: "readWrite", db: "users" } ]
 });
 
+
+
 // ID for default images is 24 of the same number, to make naming default files not insane
-for (let i = 0; i < 8; i++) {
+for (let i = 0; i < 6; i++) {
     let objID = "";
     for (let j = 0; j < 24; j++) {
         objID += String(i);
     }
     db.samoyeds.insert({
-        _id: ObjectId(objID)
+        _id: ObjectId(objID),
+        path: `/usr/src/app/api/i/${objID}`
     });
 }
 
